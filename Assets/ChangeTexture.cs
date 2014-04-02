2e9dcc494c83fa1f59fa1b58668ff520ccf20d9a
Anthony Romrell <anthonyromrell@mac.com>
2014-04-02 00:02:06 -0600

    Added Code for optimizing sprites.

---
+++ Assets/ChangeTexture.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeTexture : MonoBehaviour {
+
+	void Start () {
+		Object[] sprt = Resources.LoadAll("SD", typeof(Sprite));
+		SpriteRenderer _sprt = this.GetComponent<SpriteRenderer>();
+		foreach (Sprite _s in sprt) {
+//			Debug.Log(_s);
+//			Debug.Log(_sprt.sprite);
+			if(_sprt.sprite.name == _s.name) {
+				_sprt.sprite = _s;
+//				Debug.Log(_s);
+			}
+		}
+
+		Object sp = Resources.Load<Sprite>("HD"+_sprt.name);
+
+		Debug.Log(sp);
+	}
+}
