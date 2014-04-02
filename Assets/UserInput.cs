4214c12bd08821b79108f824718805095b0a04cf
anthonyromrell <anthonyromrell@mac.com>
2014-04-02 10:30:11 -0600

    Class Information.

---
+++ Assets/UserInput.cs
@@ -0,0 +1,25 @@
+﻿// Anthony Romrell ©2014
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UserInput : MonoBehaviour 
+{
+
+	public Animator myAnimator;
+
+	void Update () {
+		if(Input.GetKey(KeyCode.A)) {
+			myAnimator.SetBool("CanFire", true);
+		} 
+	}
+
+	void OnMouseDown ( ) {
+		myAnimator.SetBool("CanFire", true);
+	}
+
+	public void StopFire ( ) {
+		myAnimator.SetBool("CanFire", false);
+	}
+}
