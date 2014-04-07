eae660aae91bc6af473a796006a6ab42a73a62b5
anthonyromrell <anthonyromrell@mac.com>
2014-04-07 13:38:08 -0600

    Class QA

---
+++ Assets/LoadNewLevel.cs
@@ -0,0 +1,19 @@
+﻿// Anthony Romrell ©2014
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LoadNewLevel : MonoBehaviour 
+{
+
+	void OnTriggerEnter2D () {
+		Application.LoadLevel("TurtleStates");
+	}
+
+	void OnMouseDown () {
+		Application.LoadLevel("TurtleStates");
+	}
+
+
+}
