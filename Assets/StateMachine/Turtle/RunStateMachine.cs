7134688a12fa1d893299b64a444d60702f372f85
Anthony Romrell <anthonyromrell@mac.com>
2014-04-06 18:56:58 -0600

    State Machine.

---
+++ Assets/StateMachine/Turtle/RunStateMachine.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunStateMachine : MonoBehaviour {
+
+	public string myParameter;
+	public bool myBool;
+	public Animator myAnim;
+
+	void OnMouseDown () {
+		myAnim.SetBool(myParameter, myBool);
+	}
+}
