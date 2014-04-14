3ffc1f49f75c50b47a43fdfb2a2c235a7678e049
Anthony Romrell <anthonyromrell@mac.com>
2014-04-14 00:50:47 -0600

    NGUI Examples, and scripts.

---
+++ Assets/Scripts/AddToScore.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddToScore : MonoBehaviour {
+
+	public UILabel myLabel;
+	private int score = 0;
+
+	public UISlider mySlider;
+	private float myHealth = 1.0f;
+
+	void OnClick () {
+		score++;
+		if(myHealth > 0) 
+			myHealth -= 0.01f;
+		mySlider.value = myHealth;
+		myLabel.text = score.ToString();
+	}
+}
