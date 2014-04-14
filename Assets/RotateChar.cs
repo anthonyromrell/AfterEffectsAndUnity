444aeea9c67959363e9f5c04e09cf343379b8f01
anthonyromrell <anthonyromrell@mac.com>
2014-04-14 11:50:48 -0600

    Monday April 14 session.

---
+++ Assets/RotateChar.cs
@@ -0,0 +1,16 @@
+﻿// Anthony Romrell ©2014
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RotateChar : MonoBehaviour 
+{
+
+	public Animator Fox1;
+
+	void OnMouseUp () {
+		Fox1.SetBool("Rotate", true);
+	}
+
+}
---
+++ Assets/RotateOther.cs
@@ -0,0 +1,23 @@
+﻿// Anthony Romrell ©2014
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RotateOther : MonoBehaviour 
+{
+
+	public Sprite[] mySprites;
+
+
+	public Animator Fox2;
+
+	public void RotateOtherFox () {
+		Fox2.SetBool("Rotate", true);
+	}
+
+	public void EndRotate () {
+		Fox2.SetBool("Rotate", false);
+	}
+
+}
