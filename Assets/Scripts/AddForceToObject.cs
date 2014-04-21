c27fa2856ffdb3516d7772216d0f4da6e424e41f
anthonyromrell <anthonyromrell@mac.com>
2014-04-21 15:28:10 -0600

    Week 5 examples of particles and physics.

---
+++ Assets/Scripts/AddForceToObject.cs
@@ -0,0 +1,19 @@
+﻿// Anthony Romrell ©2014
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AddForceToObject : MonoBehaviour 
+{
+
+	public Rigidbody2D myRigidBody;
+	public float myForce = 30;
+
+	void Update () 
+	{
+		Vector2 _v = new Vector2(this.transform.position.x + myForce, this.transform.position.y);
+		if(Input.GetKey(KeyCode.A))
+		   myRigidBody.AddForce(_v);
+	}
+}
