e0b351c39d415d45c498049f45432f6be844afa3
anthonyromrell <anthonyromrell@mac.com>
2014-04-23 11:39:00 -0600

    Fixed Lerp

--- Assets/LerpToMove.cs
+++ Assets/LerpToMove.cs
@@ -11,33 +11,22 @@ public class LerpToMove : MonoBehaviour
 	
 	private Vector3 scanPos;
 	private Vector3 screenPoint;
-	private Vector3 offset;
-	public Vector3 curPosition;
-	private Vector3 oldPosition;
+	private Vector3 newPosition;
+	public float speed = 5;
 
 	void Start () {
-		oldPosition = myObj.position;
-		curPosition = myObj.position;
+		newPosition = myObj.position;
 	}
-
-
+	
 	void OnMouseUp () {
-
 		oldPosition = myObj.position;
-
 		screenPoint = Camera.main.WorldToScreenPoint(scanPos);
-		
 		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-		
-		curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint); //+ offset;
-		
-		//myObj.position = curPosition;
-		
-		//rigidbody.MovePosition(curPosition);
-		
+		newPosition = Camera.main.ScreenToWorldPoint(curScreenPoint); //+ offset;
+
 	}
 	
 	void Update() {
-		myObj.position = Vector3.Lerp(oldPosition, curPosition, 2);
+		myObj.position = Vector3.Lerp(myObj.position, newPosition, Time.deltaTime*speed);
 	}
 }
