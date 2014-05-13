1de041ba2c9b9ea2df374d47f9e97040821aeac0
Anthony Romrell <anthonyromrell@mac.com>
2014-05-13 00:19:54 -0600

    Week 8 AI

---
+++ Assets/NavSimpleAI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavSimpleAI : MonoBehaviour {
+
+	public NavMeshAgent myNMA;
+	public Transform myTarget;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myNMA.destination = myTarget.position;
+	}
+}
