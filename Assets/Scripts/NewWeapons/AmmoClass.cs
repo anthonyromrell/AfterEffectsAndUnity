bade23b982cd054cd2414383a19d230bdaf79c07
Anthony Romrell <anthonyromrell@mac.com>
2014-05-14 00:18:19 -0600

    Added 2D Weapons control and switching.

---
+++ Assets/Scripts/NewWeapons/AmmoClass.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class AmmoClass : MonoBehaviour {
+
+	public static Action<AmmoClass> AddAmmoToList;
+
+	public Animator ammoAnim;
+	public WeaponType.weaponSelection thisWeaponSelection;
+	//public float ammoSpeed = 2; 
+
+	void Start () {
+		if(AddAmmoToList != null)
+			AddAmmoToList(this);
+	}
+	
+	public void OnActivateAmmo (Vector3 _fireLocation, Vector3 _fireDirection, float _rotationAngle, int _ammoSpeed) {
+		transform.localEulerAngles = new Vector3(0,_rotationAngle,0);
+		rigidbody.velocity = Vector3.zero;//stops all motion of a rigid body
+		transform.position = _fireLocation;//resets the ammo gameobjects position
+		ammoAnim.SetBool("Restart", true);//sets the animators restart custom perameter
+		gameObject.SetActive(true);
+		rigidbody.AddRelativeForce(_fireDirection*_ammoSpeed, ForceMode.Impulse);//adds force to the ammos rigidBody
+	}	
+}
---
+++ Assets/Scripts/NewWeapons/WeaponList.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class WeaponList : MonoBehaviour {
+
+	public List<WeaponClass> avaliableWeapons;
+	public static Action ActivateWeaponSwitch;
+	public int activeCount = 1;
+	public int currentWeaponNum = 0;
+
+	// Use this for initialization
+	void Start () {
+		avaliableWeapons = new List<WeaponClass>();
+		WeaponClass.AddWeaponToList += AddWeapons;
+		SwitchCurrentWeapon.SwitchWeapon += SwitchThisWeapon;
+		FireThisWeapon.FireCurrentWeapon += FireWeapon;
+	}
+
+	void AddWeapons (WeaponClass _w) {
+		avaliableWeapons.Add(_w);
+		if(avaliableWeapons.Count > activeCount) 
+			if(ActivateWeaponSwitch != null)
+				ActivateWeaponSwitch();
+	}
+
+	void SwitchThisWeapon () {
+		if(avaliableWeapons.Count-1 > currentWeaponNum) {
+			currentWeaponNum++;
+		} else {
+			currentWeaponNum = 0;
+		}
+	}
+
+	void FireWeapon () {
+		avaliableWeapons[currentWeaponNum].FireAmmo();
+	} 
+}
