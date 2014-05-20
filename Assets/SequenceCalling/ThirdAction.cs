6fa665e15ab94c06a50a4e0fddad8dc8ef1aaa29
anthonyromrell <anthonyromrell@mac.com>
2014-05-20 10:36:22 -0600

    Sequencing Objects

---
+++ Assets/SequenceCalling/ThirdAction.cs
@@ -0,0 +1,24 @@
+﻿// Anthony Romrell ©2014
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ThirdAction : MonoBehaviour 
+{
+	public Animator myAnimator;
+
+	void OnEnable () 
+	{
+		SecondAction.mySecondAction += ThirdFunction;
+	}
+	
+	void ThirdFunction () 
+	{
+		myAnimator.SetBool("ifCalled", true);
+	}
+	
+	void AnimEndFunction () {
+		myAnimator.SetBool("ifCalled", false);
+	}
+}
